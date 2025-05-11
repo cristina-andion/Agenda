297f2d43901023d7dc81677c2af37ec8c4ec84cc
cristina-andion <cristina.andion@gmail.com>
2025-05-11 23:48:20 +0200

    Control errors per camps buits al formulari

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ li.textContent = nom;
 llista.appendChild(li);
 });
 document.body.appendChild(llista);
+document.querySelector("form").addEventListener("submit", e => {
+e.preventDefault();
+const input = document.querySelector("#nom");
+if (input.value.trim() === "") {
+alert("El camp no pot estar buit");
+return;
+}
+// Lògica per afegir el contacte...
+});
